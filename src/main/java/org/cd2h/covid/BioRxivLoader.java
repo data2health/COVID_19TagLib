4f6425b4118a549a83d97ff53d143a41981df4dd
eichmann <david-eichmann@uiowa.edu>
2020-03-25 22:17:58 +0000

    Full text extraction from the PDF in place.

--- src/main/java/org/cd2h/covid/BioRxivLoader.java
+++ src/main/java/org/cd2h/covid/BioRxivLoader.java
@@ -1,6 +1,7 @@
 package org.cd2h.covid;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -16,12 +17,17 @@ import java.util.Properties;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.sax.BodyContentHandler;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.xml.sax.SAXException;
 
 import edu.uiowa.slis.GitHubTagLib.util.LocalProperties;
 import edu.uiowa.slis.GitHubTagLib.util.PropertyLoader;
@@ -33,15 +39,19 @@ public class BioRxivLoader {
     static int apiLimit = 100;
     static String filePrefix = "/Volumes/Pegasus0/COVID/";
 
-    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
-	PropertyConfigurator.configure(args[0]);
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException, InterruptedException, SAXException, TikaException {
+	System.setProperty("java.awt.headless", "true");
+
+	PropertyConfigurator.configure("/Users/eichmann/Documents/Components/log4j.info");
 	initialize();
 
 //	scan_feed();
 
 //	fetch();
 	
-	scan_html();
+//	scan_html();
+	scan_pdf();
+//	logger.info(parseToPlainText(filePrefix+"2020.03.10.20033852v1.full.pdf"));
 
 	// simpleStmt("refresh materialized view covid.book_collection");
 	// simpleStmt("refresh materialized view covid.book_creator");
@@ -99,8 +109,8 @@ public class BioRxivLoader {
 	simpleStmt("refresh materialized view covid.biorxiv_current");
     }
     
-    static public void scan_html() throws SQLException, IOException {
-	PreparedStatement fetchStmt = conn.prepareStatement("select doi,link from covid.biorxiv_current");
+    static public void scan_html() throws SQLException, IOException, InterruptedException {
+	PreparedStatement fetchStmt = conn.prepareStatement("select doi,link from covid.biorxiv_current where doi not in (select doi from covid.biorxiv_map)");
 	ResultSet rs = fetchStmt.executeQuery();
 	while (rs.next()) {
 	    String doi = rs.getString(1);
@@ -108,9 +118,9 @@ public class BioRxivLoader {
 	    logger.info("doi: " + doi + "\tlink: " + link);
 	    Document doc = Jsoup.connect(link).timeout(0).get();
 	    Element element = doc.getElementsByClass("article-dl-pdf-link").first();
-	    logger.info("element: " + element);
+	    logger.trace("element: " + element);
 	    String href = element.attr("href");
-	    logger.info("\thref: " + href);
+	    logger.trace("\thref: " + href);
 	    
 	    String old_href = null;
 	    PreparedStatement urlStmt = conn.prepareStatement("select url from covid.biorxiv_map where doi = ?");
@@ -129,7 +139,7 @@ public class BioRxivLoader {
 		citeStmt.executeUpdate();
 		citeStmt.close();
 		URL fetchURL = new URL(new URL(link.replace("http", "https")), href);
-		logger.info("\t\tfetching... " + fetchURL);
+		logger.info("\tfetching... " + fetchURL);
 		Files.copy(fetchURL.openStream(), Paths.get(filePrefix+fetchURL.getFile().substring(fetchURL.getFile().lastIndexOf('/')+1)), StandardCopyOption.REPLACE_EXISTING);
 	    } else if (!href.equals(old_href)) {
 		// updated entry
@@ -139,13 +149,46 @@ public class BioRxivLoader {
 		citeStmt.executeUpdate();
 		citeStmt.close();
 		URL fetchURL = new URL(new URL(link.replace("http", "https")), href);
-		logger.info("\t\tfetching... " + fetchURL);
+		logger.info("\tfetching... " + fetchURL);
 		Files.copy(fetchURL.openStream(), Paths.get(filePrefix+fetchURL.getFile().substring(fetchURL.getFile().lastIndexOf('/')+1)), StandardCopyOption.REPLACE_EXISTING);
 	    } else {
-		logger.info("\t\tskipping...");
+		logger.info("\tskipping...");
 	    }
+	    Thread.sleep(5000);
 	}
 	fetchStmt.close();
+    }
+
+    static public void scan_pdf() throws SQLException, IOException, SAXException, TikaException {
+	PreparedStatement fetchStmt = conn
+		.prepareStatement("select doi,url from covid.biorxiv_map where doi not in (select doi from covid.biorxiv_text)");
+	ResultSet rs = fetchStmt.executeQuery();
+	while (rs.next()) {
+	    String doi = rs.getString(1);
+	    String fetchURL = rs.getString(2);
+	    String file = filePrefix+fetchURL.substring(fetchURL.lastIndexOf('/')+1);
+	    logger.info("DOI: " + doi + "\tfile: " + file);
+	    
+	    String contents = parseToPlainText(file);
+		PreparedStatement citeStmt = conn.prepareStatement("insert into covid.biorxiv_text values (?,?)");
+		citeStmt.setString(1, doi);
+		citeStmt.setString(2, contents);
+		citeStmt.executeUpdate();
+		citeStmt.close();
+
+	}
+	// for (int i = 0; i < resultArray.length(); i++) {
+	// if (resultArray.isNull(i))
+	// continue;
+	// JSONObject theObject = resultArray.getJSONArray(i).getJSONObject(0);
+	// logger.info("object: " + theObject.toString(3));
+	//
+	// PreparedStatement citeStmt = conn.prepareStatement("insert into
+	// covid.raw_biorxiv values (?::jsonb)");
+	// citeStmt.setString(1, theObject.toString());
+	// citeStmt.executeUpdate();
+	// citeStmt.close();
+	// }
     }
 
     static public void fetch() throws SQLException, IOException {
@@ -184,6 +227,21 @@ public class BioRxivLoader {
 	// citeStmt.executeUpdate();
 	// citeStmt.close();
 	// }
+    }
+
+    static public String parseToPlainText(String path) throws IOException, SAXException, TikaException {
+	BodyContentHandler handler = new BodyContentHandler(-1);
+	FileInputStream str = new FileInputStream(path);
+	AutoDetectParser parser = new AutoDetectParser();
+	Metadata metadata = new Metadata();
+	parser.parse(str, handler, metadata);
+	
+//	String[] metadataNames = metadata.names();
+//	for (String name : metadataNames) {
+//	    System.out.println(name + " : " + metadata.get(name));
+//	}
+
+	return handler.toString();
     }
 
     static public void initialize() throws ClassNotFoundException, SQLException {
