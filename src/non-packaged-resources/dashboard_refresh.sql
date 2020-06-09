c8fc54057c5eff490f74e888e12f111ec2acf4a8
eichmann <david-eichmann@uiowa.edu>
2020-06-09 22:01:56 +0000

    Tuning up refresh behavior.

--- src/non-packaged-resources/dashboard_refresh.sql
+++ src/non-packaged-resources/dashboard_refresh.sql
@@ -35,3 +35,39 @@ insert into n3c_admin.irb
 
 delete from n3c_admin.mapping_ncats
 where n3c not in (select site from n3c_admin.n3c) or ncats not in (select site_name from n3c_admin.ncats);
+
+create materialized view n3c_admin.personnel as
+select
+	timestamp::timestamp as onboarded,
+	email_address,
+	first_name,
+	last_name,
+    google__g_suite_enabled_email__if_different_from_primary__pr as gmail,
+    slack_email_if_different_from_primary__previously_entered__a as slack,
+    github_handle as github,
+    case when substring(orcid_id from '[0-9]+-[0-9]+-[0-9]+-[0-9X]+') = '0000-0000-0000-0000' then null else substring(orcid_id from '[0-9]+-[0-9]+-[0-9]+-[0-9X]+') end as orcid,
+    assistant_email,
+    institutional_affiliation__ctsa__or_employer__url_ as institution_url,
+    root_domain__calculated_ as root_domain,
+    institution_lookup,
+	would_you_like_to_onboard_to_n3c_::boolean as n3c_onboard,
+	would_you_like_to_onboard_to_cd2h_::boolean as cd2h_onboard,
+    substring(n3c_all_hands from '^[^ ]+')::boolean as n3c_all_hands,
+    substring(data_partnership___governance from '^[^ ]+')::boolean as governance,
+    substring(phenotype___data_acquisition from '^[^ ]+')::boolean as data_acquisition,
+    substring(data_ingestion___harmonization from '^[^ ]+')::boolean as harmonization,
+    substring(collaborative_analytics from '^[^ ]+')::boolean as collaborative_analytics,
+    substring(synthetic_data from '^[^ ]+')::boolean as synthetic_data,
+    substring(portal___dashboards__collaborative_analytics__subgroup from '^[^ ]+')::boolean as portal_dashboards,
+    substring(tools___resources__collaborative_analytics__subgroup from '^[^ ]+')::boolean as tools_resources,
+    substring(clinical_scenarios___data_analytics__collaborative_analytics from '^[^ ]+')::boolean as clinical_scenarios,
+    substring(nlp__collaborative_analytics_ from '^[^ ]+')::boolean as nlp,
+    would_you_like_to_onboard_to_cd2h__1::boolean as cd2h_onboard2,
+    substring(cd2h_all_hands from '^[^ ]+')::boolean as cd2h_all_hands,
+    substring(tools___cloud_infrastructure from '^[^ ]+')::boolean as tools_cloud,
+    substring(next_generation_data_sharing from '^[^ ]+')::boolean as next_gen_data,
+    substring(informatics_maturity_and_best_practices from '^[^ ]+')::boolean as maturity,
+    substring(resource_discovery from '^[^ ]+')::boolean as resource_discovery
+from n3c_admin.newformmaster
+where email_address is not null
+;
