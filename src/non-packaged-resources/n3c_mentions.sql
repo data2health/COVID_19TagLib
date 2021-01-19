37c43b8e0c510697b2e18a06d789e8b701937f47
eichmann <david-eichmann@uiowa.edu>
2021-01-19 23:09:35 +0000

    Working on views for UMLS recognition in preprints.

--- src/non-packaged-resources/n3c_mentions.sql
+++ src/non-packaged-resources/n3c_mentions.sql
@@ -30,3 +30,20 @@ select distinct doi, seqnum, name, sentnum, full_text, original
 from covid_biorxiv.sentence natural join covid_biorxiv.section, covid_biorxiv.cohort_med
 where full_text ~ original or full_text ~ normalized
 ;
+
+CREATE MATERIALIZED VIEW umls_local.disease AS
+SELECT
+	mrsty.cui,
+    mrsty.stn,
+    mrsty.sty,
+    uistr.str,
+    ((('(^|[^a-zA-Z])[' || lower(substring(uistr.str, 1, 1))) || upper(substring(uistr.str, 1, 1))) || ']') || lower(substring(uistr.str, 2))||'([^a-zA-Z0-9]|$)' AS normalized
+FROM umls.mrsty NATURAL JOIN uistr
+WHERE mrsty.stn ~ '^B2.2.1.2'
+  AND uistr.str ~ '^[a-zA-Z0-9 ]+$';
+
+create materialized view covid_biorxiv.umls_disease_match as
+select doi,seqnum,sentnum,cui,str
+from covid_biorxiv.sentence,disease
+where full_text ~ normalized
+;
