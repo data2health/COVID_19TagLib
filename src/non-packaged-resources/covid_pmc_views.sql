00f1ef39e88165d118ace37f1f8e1e62d69285ac
eichmann <david-eichmann@uiowa.edu>
2021-02-23 23:19:55 +0000

    More progress on chained XML scrubbing. XML views and scrubbing views are now in separate files.

---
+++ src/non-packaged-resources/covid_pmc_views.sql
@@ -0,0 +1,216 @@
+-- a cascading chain of views to scrub XML markup from PMC article paragraphs
+
+drop view sec_para_filter cascade;
+
+create view sec_para_filter as
+select regexp_replace(p::text, '<p[^>]*>(.*)</p>', '\1') as p
+from section_paragraph;
+
+create view sec_para_sup1_filter as
+select
+	regexp_replace(p, '<sup>([^<]*)</sup>', '\1', 'g') as p
+from sec_para_filter;
+
+create view sec_para_italic1_filter as
+select
+	regexp_replace(p, '<italic>([^<]*)</italic>', '\1', 'g') as p
+from sec_para_sup1_filter;
+
+create view sec_para_bib_filter as
+select
+	regexp_replace(p, '([\[(]<xref[^>]*bibr[^>]*>[^<]*</xref>( *[,;] *<xref[^>]*bibr[^>]*>[^<]*</xref>)*[\])])', '', 'g') as p
+from sec_para_italic1_filter;
+
+create view sec_para_bib2_filter as
+select
+	regexp_replace(p, '(<xref[^>]*bibr[^>]*>[^<]*</xref>( *[,;] *<xref[^>]*bibr[^>]*>[^<]*</xref>)*)', '', 'g') as p
+from sec_para_bib_filter;
+
+create view sec_para_xref_filter as
+select
+	regexp_replace(p, '<xref[^>]*>([^<]*)</xref>', '\1', 'g') as p
+from sec_para_bib2_filter;
+
+create view sec_para_fig_filter as
+select
+	regexp_replace(p, '<fig [^>]*>.*</fig>', '', 'g') as p
+from sec_para_xref_filter;
+
+create view sec_para_table_filter as
+select
+	regexp_replace(p, '<table-wrap [^>]*>.*</table-wrap>', '', 'g') as p
+from sec_para_fig_filter;
+
+create view sec_para_sub_filter as
+select
+	regexp_replace(p, '<sub>([^<]*)</sub>', '\1', 'g') as p
+from sec_para_table_filter;
+
+create view sec_para_sup_filter as
+select
+	regexp_replace(p, '<sup>([^<]*)</sup>', '\1', 'g') as p
+from sec_para_sub_filter;
+
+create view sec_para_link_filter as
+select
+	regexp_replace(p, '\(<ext-link ([^<]*)</ext-link>\)', '', 'g') as p
+from sec_para_sup_filter;
+
+create view sec_para_math_space_filter as
+select
+	regexp_replace(p, '<mml:mspace[^>]*/>', ' ', 'g') as p
+from sec_para_link_filter;
+
+create view sec_para_math_text_filter as
+select
+	regexp_replace(p, '<mml:mtext[^>]*>([^<]*)</mml:mtext>', '\1', 'g') as p
+from sec_para_math_space_filter;
+
+create view sec_para_math_element_filter as
+select
+	regexp_replace(p, '<mml:m[ion][^>]*>([^<]*)</mml:m[ion]>', '\1', 'g') as p
+from sec_para_math_text_filter;
+
+create view sec_para_math_sup_filter as
+select
+	regexp_replace(p, '<mml:msup[^>]*>([^<]*)</mml:msup>', '\1', 'g') as p
+from sec_para_math_element_filter;
+
+create view sec_para_math_sub_filter as
+select
+	regexp_replace(p, '<mml:msub[^>]*>([^<]*)</mml:msub>', '\1', 'g') as p
+from sec_para_math_sup_filter;
+
+create view sec_para_math_subsup_filter as
+select
+	regexp_replace(p, '<mml:msubsup[^>]*>([^<]*)</mml:msubsup>', '\1', 'g') as p
+from sec_para_math_sub_filter;
+
+create view sec_para_math_frac_filter as
+select
+	regexp_replace(p, '<mml:mfrac[^>]*>([^<]*)</mml:mfrac>', '\1', 'g') as p
+from sec_para_math_subsup_filter;
+
+create view sec_para_math_sqrt_filter as
+select
+	regexp_replace(p, '<mml:msqrt[^>]*>([^<]*)</mml:msqrt>', '\1', 'g') as p
+from sec_para_math_frac_filter;
+
+create view sec_para_math_under_filter as
+select
+	regexp_replace(p, '<mml:munder[^>]*>([^<]*)</mml:munder>', '\1', 'g') as p
+from sec_para_math_sqrt_filter;
+
+create view sec_para_math_row_filter as
+select
+	regexp_replace(p, '<mml:mrow[^>]*>([^<]*)</mml:mrow>', '\1', 'g') as p
+from sec_para_math_under_filter;
+
+create view sec_para_row_sup_filter as
+select
+	regexp_replace(p, '<mml:msup[^>]*>([^<]*)</mml:msup>', '\1', 'g') as p
+from sec_para_math_row_filter;
+
+create view sec_para_row_sub_filter as
+select
+	regexp_replace(p, '<mml:msub[^>]*>([^<]*)</mml:msub>', '\1', 'g') as p
+from sec_para_row_sup_filter;
+
+create view sec_para_row_subsup_filter as
+select
+	regexp_replace(p, '<mml:msubsup[^>]*>([^<]*)</mml:msubsup>', '\1', 'g') as p
+from sec_para_row_sub_filter;
+
+create view sec_para_row_frac_filter as
+select
+	regexp_replace(p, '<mml:mfrac[^>]*>([^<]*)</mml:mfrac>', '\1', 'g') as p
+from sec_para_row_subsup_filter;
+
+create view sec_para_row_sqrt_filter as
+select
+	regexp_replace(p, '<mml:msqrt[^>]*>([^<]*)</mml:msqrt>', '\1', 'g') as p
+from sec_para_row_subsup_filter;
+
+create view sec_para_row_under_filter as
+select
+	regexp_replace(p, '<mml:munder[^>]*>([^<]*)</mml:munder>', '\1', 'g') as p
+from sec_para_row_sqrt_filter;
+
+create view sec_para_row_row_filter as
+select
+	regexp_replace(p, '<mml:mrow[^>]*>([^<]*)</mml:mrow>', '\1', 'g') as p
+from sec_para_row_under_filter;
+
+create view sec_para_math_math_filter as
+select
+	regexp_replace(p, '<mml:math[^>]*>([^<]*)</mml:math>', '\1', 'g') as p
+from sec_para_row_row_filter;
+
+create view sec_para_tex_math_filter as
+select
+	regexp_replace(p, '<tex-math[^>]*>([^<]*)</tex-math>', '', 'g') as p
+from sec_para_math_math_filter;
+
+create view sec_para_inline_graphic_filter as
+select
+	regexp_replace(p, '<(inline-)?graphic [^/]*/>', '', 'g') as p
+from sec_para_tex_math_filter;
+
+create view sec_para_alternatives_filter as
+select
+	regexp_replace(p, '<alternatives>([^<]*)</alternatives>', '', 'g') as p
+from sec_para_inline_graphic_filter;
+
+create view sec_para_math_formula_filter as
+select
+	regexp_replace(p, '<inline-formula[^>]*>([^<]*)</inline-formula>', '\1', 'g') as p
+from sec_para_alternatives_filter;
+
+create view sec_para_label_filter as
+select
+	regexp_replace(p, '<label>([^<]*)</label>', '\1', 'g') as p
+from sec_para_math_formula_filter;
+
+create view sec_para_dformula_filter as
+select
+	regexp_replace(p, '<disp-formula[^>]*>([^<]*)</disp-formula>', '\1', 'g') as p
+from sec_para_label_filter;
+
+create view sec_para_dgroup_filter as
+select
+	regexp_replace(p, '<disp-formula-group[^>]*>([^<]*)</disp-formula-group>', '', 'g') as p
+from sec_para_dformula_filter;
+
+create view sec_para_sub_filter as
+select
+	regexp_replace(p, '<sub>([^<]*)</sub>', '\1', 'g') as p
+from sec_para_dgroup_filter;
+
+create view sec_para_sup_filter as
+select
+	regexp_replace(p, '<sup>([^<]*)</sup>', '\1', 'g') as p
+from sec_para_sub_filter;
+
+create view sec_para_italic_filter as
+select
+	regexp_replace(p, '<italic>([^<]*)</italic>', '\1', 'g') as p
+from sec_para_sup_filter;
+
+create view sec_para_bold_filter as
+select
+	regexp_replace(p, '<bold>([^<]*)</bold>', '\1', 'g') as p
+from sec_para_italic_filter;
+
+create view sec_para_sc_filter as
+select
+	regexp_replace(p, '<sc>([^<]*)</sc>', '\1', 'g') as p
+from sec_para_bold_filter;
+
+create view sec_para_email_filter as
+select
+	regexp_replace(p, '<email>([^<]*)</email>', '\1', 'g') as p
+from sec_para_sc_filter;
+
+
+select p from sec_para_email_filter
+limit 1000;
