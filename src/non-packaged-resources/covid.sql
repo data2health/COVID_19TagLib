d93c6e1943e1d55029dd26c17f0eb5f68c45aaca
eichmann <david-eichmann@uiowa.edu>
2021-03-15 14:01:22 +0000

    Unified scheme for sentence processing now in place.

--- src/non-packaged-resources/covid.sql
+++ src/non-packaged-resources/covid.sql
@@ -1,7 +1,7 @@
-create view covid.sentence as
+create view covid.sentence_staging as
 select distinct * from (
 	select
-		'biorxiv' as source,
+		(select site from covid_biorxiv.biorxiv_current where biorxiv_current.doi = sentence.doi) as source,
 		doi,
 		null::int as pmcid,
 		null::int as pmid,
@@ -48,3 +48,81 @@ select distinct * from (
 		string as sentence
 	from covid_litcovid.sentence
 ) as foo;
+
+create view covid.sentence_filter as
+select
+	source,
+	coalesce(doi, '') as doi,
+	coalesce(pmcid, -1) as pmcid,
+	coalesce(pmid, -1) as pmid,
+	coalesce(seqnum, -1) as seqnum,
+	section,
+	coalesce(seqnum2, -1) as seqnum2,
+	coalesce(seqnum3, -1) as seqnum3,
+	coalesce(seqnum4, -1) as seqnum4,
+	coalesce(seqnum5, -1) as seqnum5,
+	coalesce(seqnum6, -1) as seqnum6,
+	coalesce(sentnum, -1) as sentnum,
+	sentence
+from covid.sentence_staging;
+
+create materialized view covid.sentence_ncats_match as
+select doi,pmcid,pmid,seqnum,seqnum2,seqnum3,seqnum4,seqnum5,seqnum6,sentnum,id
+from covid.sentence_staging,covid_ncats.medication
+where sentence~pattern
+;
+
+create index sent_doi on covid.sentence_ncats_match(doi);
+create index sent_pmcid on covid.sentence_ncats_match(pmcid);
+create index sent_pmid on covid.sentence_ncats_match(pmid);
+create index sent_id on covid.sentence_ncats_match(id);
+
+create view covid.sentence_ncats_match_filter as
+select
+	coalesce(doi, '') as doi,
+	coalesce(pmcid, -1) as pmcid,
+	coalesce(pmid, -1) as pmid,
+	coalesce(seqnum, -1) as seqnum,
+	coalesce(seqnum2, -1) as seqnum2,
+	coalesce(seqnum3, -1) as seqnum3,
+	coalesce(seqnum4, -1) as seqnum4,
+	coalesce(seqnum5, -1) as seqnum5,
+	coalesce(seqnum6, -1) as seqnum6,
+	coalesce(sentnum, -1) as sentnum,
+	id	
+from covid.sentence_ncats_match;
+
+create materialized view covid.sentence as
+select
+	source,
+	doi,
+	pmcid,
+	pmid,
+	case
+		when source = 'biorxiv' then (select title from covid_biorxiv.document where document.doi = sentence_filter.doi)
+		when source = 'litcovid' then (select article_title from covid_litcovid.article_title where article_title.pmid = sentence_filter.pmid)
+		when source = 'pmc' then (select article_title from covid_litcovid.article_title natural join covid_pmc.xml_link where xml_link.pmcid = sentence_filter.pmcid limit 1)
+		else ''
+	end as title,
+	case
+		when source = 'biorxiv' then 'http://dx.doi.org/'||doi
+		when source = 'litcovid' and exists (select article_id from covid_litcovid.article_id where article_id.pmid = sentence_filter.pmid and id_type='doi' limit 1)
+			then 'http://dx.doi.org/'||(select article_id from covid_litcovid.article_id where article_id.pmid = sentence_filter.pmid and id_type='doi' limit 1)
+		when source = 'litcovid' then 'https://pubmed.ncbi.nlm.nih.gov/'||pmid
+		when source = 'pmc' and exists (select article_id from covid_litcovid.article_id natural join covid_pmc.xml_link where xml_link.pmcid = sentence_filter.pmcid and id_type='doi' limit 1)
+		    then 'http://dx.doi.org/'||(select article_id from covid_litcovid.article_id natural join covid_pmc.xml_link where xml_link.pmcid = sentence_filter.pmcid and id_type='doi' limit 1)
+		when source = 'pmc' then 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC'||pmcid
+		else ''
+	end as url,
+	section,
+	medication,
+	regexp_replace(sentence, '('||pattern||')', '<b>\1</b>', 'i') as sentence
+from covid.sentence_ncats_match_filter
+natural join covid_ncats.medication
+natural join covid.sentence_filter
+;
+
+create index s_doi on covid.sentence(doi);
+create index s_pmcid on covid.sentence(pmcid);
+create index s_pmid on covid.sentence(pmid);
+create index s_med on covid.sentence(medication);
--- src/non-packaged-resources/covid_ncats.sql
+++ src/non-packaged-resources/covid_ncats.sql
@@ -1,7 +1,12 @@
+create view medication as
 select
 	id,
-	regexp_replace(regexp_replace(med_name__generic_, ' exclud.*', ''), ' [iI]njectable.*', '') as med,
-	(select max(foo.id) from medications_concept_sets as foo where class__atc_ is not null and foo.id < bar.id) as parent
+	regexp_replace(regexp_replace(med_name__generic_, ' exclud.*', ''), ' [iI]njectable.*', '') as medication,
+	'['||lower(substring(med_name__generic_ from 1 for 1))||upper(substring(med_name__generic_ from 1 for 1))||']'||substring(med_name__generic_ from 2) as pattern,
+	priority,
+	rxcui,
+	(select max(foo.id) from medications_concept_sets as foo where class__atc_ is not null and foo.id < bar.id) as class_id,
+	(select class__atc_ from medications_concept_sets where id in (select max(foo.id) from medications_concept_sets as foo where class__atc_ is not null and foo.id < bar.id)) as class_name
 from medications_concept_sets as bar
 where class__atc_ is null
   and med_name__generic_ is not null;
