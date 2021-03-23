ce8338e0ba94e8869bc548b01dc274dfe233afe4
eichmann <david-eichmann@uiowa.edu>
2021-03-23 13:56:56 +0000

    Much work on the refactoring to the new scheme.

--- src/non-packaged-resources/covid_n3c.sql
+++ src/non-packaged-resources/covid_n3c.sql
@@ -14,6 +14,35 @@ where sentence ~ normalized
    or sentence ~ original
 ;
 
+create materialized view covid_n3c.sentence2 as
+select
+	source,
+	doi,
+	pmcid,
+	pmid,
+	case
+		when source = 'biorxiv' then (select title from covid_biorxiv.document where document.doi = sentence_filter.doi)
+		when source = 'litcovid' then (select article_title from covid_litcovid.article_title where article_title.pmid = sentence_filter.pmid)
+		when source = 'pmc' then (select article_title from covid_litcovid.article_title natural join covid_pmc.xml_link where xml_link.pmcid = sentence_filter.pmcid limit 1)
+		else ''
+	end as title,
+	case
+		when source = 'biorxiv' then 'http://dx.doi.org/'||doi
+		when source = 'litcovid' and exists (select article_id from covid_litcovid.article_id where article_id.pmid = sentence_filter.pmid and id_type='doi' limit 1)
+			then 'http://dx.doi.org/'||(select article_id from covid_litcovid.article_id where article_id.pmid = sentence_filter.pmid and id_type='doi' limit 1)
+		when source = 'litcovid' then 'https://pubmed.ncbi.nlm.nih.gov/'||pmid
+		when source = 'pmc' and exists (select article_id from covid_litcovid.article_id natural join covid_pmc.xml_link where xml_link.pmcid = sentence_filter.pmcid and id_type='doi' limit 1)
+		    then 'http://dx.doi.org/'||(select article_id from covid_litcovid.article_id natural join covid_pmc.xml_link where xml_link.pmcid = sentence_filter.pmcid and id_type='doi' limit 1)
+		when source = 'pmc' then 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC'||pmcid
+		else ''
+	end as url,
+	section,
+	original,
+	regexp_replace(sentence, '('||normalized||')', '<b>\1</b>', 'i') as sentence
+from covid_n3c.cohort_med, covid.sentence_filter
+where sentence ~ normalized
+;
+
 create materialized view covid_n3c.drugs_by_week as 
 select distinct
 	source,
