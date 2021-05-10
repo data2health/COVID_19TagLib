13d955497c4b76f4d954bb556e8c420a430b494e
eichmann <david-eichmann@uiowa.edu>
2021-05-10 17:45:52 +0000

    Fixed nested table references for PubChem.

--- src/non-packaged-resources/covid_sentence_refresh_pubchem_compounds.sql
+++ src/non-packaged-resources/covid_sentence_refresh_pubchem_compounds.sql
@@ -4,16 +4,16 @@
 \echo -n 'by DOI...     '
 insert into covid_pubchem.sentence_compound select * from covid_pubchem.sentence_compound_staging
 where doi is not null
-  and exists (select doi from covid_pubchem.refresh_queue where sentence_staging.doi=refresh_queue.doi)
+  and exists (select doi from covid_pubchem.refresh_queue where sentence_compound_staging.doi=refresh_queue.doi)
   and not exists (select doi from covid_pubchem.sentence_compound where sentence_compound.doi=sentence_compound_staging.doi);
 \echo -n 'by PMCID...   '
 insert into covid_pubchem.sentence_compound select * from covid_pubchem.sentence_compound_staging
 where pmcid > 0
-  and exists (select pmcid from covid_pubchem.refresh_queue where sentence_staging.pmcid=refresh_queue.pmcid)
+  and exists (select pmcid from covid_pubchem.refresh_queue where sentence_compound_staging.pmcid=refresh_queue.pmcid)
   and not exists (select pmcid from covid_pubchem.sentence_compound where sentence_compound.pmcid=sentence_compound_staging.pmcid);
 \echo -n 'by PMID...    '
 insert into covid_pubchem.sentence_compound select * from covid_pubchem.sentence_compound_staging
 where pmid > 0
-  and exists (select pmid from covid_pubchem.refresh_queue where sentence_staging.pmid=refresh_queue.pmid)
+  and exists (select pmid from covid_pubchem.refresh_queue where sentence_compound_staging.pmid=refresh_queue.pmid)
   and not exists (select pmid from covid_pubchem.sentence_compound where sentence_compound.pmid=sentence_compound_staging.pmid);
 refresh materialized view covid_pubchem.compounds_drugs_by_week;
--- src/non-packaged-resources/covid_sentence_refresh_pubchem_substances.sql
+++ src/non-packaged-resources/covid_sentence_refresh_pubchem_substances.sql
@@ -4,16 +4,16 @@
 \echo -n 'by DOI...     '
 insert into covid_pubchem.sentence_substance select * from covid_pubchem.sentence_substance_staging
 where doi is not null
-  and exists (select doi from covid_pubchem.refresh_queue where sentence_staging.doi=refresh_queue.doi)
+  and exists (select doi from covid_pubchem.refresh_queue where sentence_substance_staging.doi=refresh_queue.doi)
   and not exists (select doi from covid_pubchem.sentence_substance where sentence_substance.doi=sentence_substance_staging.doi);
 \echo -n 'by PMCID...   '
 insert into covid_pubchem.sentence_substance select * from covid_pubchem.sentence_substance_staging
 where pmcid > 0
-  and exists (select pmcid from covid_pubchem.refresh_queue where sentence_staging.pmcid=refresh_queue.pmcid)
+  and exists (select pmcid from covid_pubchem.refresh_queue where sentence_substance_staging.pmcid=refresh_queue.pmcid)
   and not exists (select pmcid from covid_pubchem.sentence_substance where sentence_substance.pmcid=sentence_substance_staging.pmcid);
 \echo -n 'by PMID...    '
 insert into covid_pubchem.sentence_substance select * from covid_pubchem.sentence_substance_staging
 where pmid > 0
-  and exists (select pmid from covid_pubchem.refresh_queue where sentence_staging.pmid=refresh_queue.pmid)
+  and exists (select pmid from covid_pubchem.refresh_queue where sentence_substance_staging.pmid=refresh_queue.pmid)
   and not exists (select pmid from covid_pubchem.sentence_substance where sentence_substance.pmid=sentence_substance_staging.pmid);
 refresh materialized view covid_pubchem.substances_drugs_by_week;
